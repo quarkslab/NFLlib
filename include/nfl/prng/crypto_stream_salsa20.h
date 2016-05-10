b64e113716f2bdaedc020981eabaa3d98737d973
Tancrède Lepoint <tancrede.lepoint@cryptoexperts.com>
2016-05-10 17:06:17 +0200

    Minor modifications

--- include/nfl/prng/crypto_stream_salsa20.h
+++ include/nfl/prng/crypto_stream_salsa20.h
@@ -1,30 +1,12 @@
 #ifndef CRYPTO_STREAM_H
 #define CRYPTO_STREAM_H
 
-#ifdef __cplusplus
 #include <string>
-#endif
 
-namespace nfl
-{
-#define crypto_stream_salsa20_KEYBYTES 32
-#define crypto_stream_salsa20_NONCEBYTES 8
-
-#ifdef __cplusplus
 extern "C" {
-#endif
-extern
-int crypto_stream_salsa20_amd64_xmm6(
-        unsigned char *c,unsigned long long clen,
-  const unsigned char *n,
-  const unsigned char *k
-);
-    
-#ifdef __cplusplus
-}
-#endif
-#define crypto_stream_salsa20 crypto_stream_salsa20_amd64_xmm6
-
+int nfl_crypto_stream_salsa20_amd64_xmm6(unsigned char *c, unsigned long long clen,
+                                     const unsigned char *n,
+                                     const unsigned char *k);
 }
 
 #endif
--- lib/prng/fastrandombytes.cpp
+++ lib/prng/fastrandombytes.cpp
@@ -4,35 +4,29 @@
  * Public Domain
  */
 
-#include "nfl/prng/crypto_stream_salsa20.h"
-#include "nfl/prng/randombytes.h"
 #include <inttypes.h>
 #include <iostream>
+#include "nfl/prng/crypto_stream_salsa20.h"
+#include "nfl/prng/randombytes.h"
 
 namespace nfl {
 
 static int init = 0;
-static unsigned char key[crypto_stream_salsa20_KEYBYTES];
-static unsigned char nonce[crypto_stream_salsa20_NONCEBYTES] = {0};
+static unsigned char key[32];
+static unsigned char nonce[8] = {0};
 
-void fastrandombytes(unsigned char *r, unsigned long long rlen)
-{
-  unsigned long long n=0;
+void fastrandombytes(unsigned char *r, unsigned long long rlen) {
+  unsigned long long n = 0;
   int i;
-  if(!init)
-  {
-    randombytes(key, crypto_stream_salsa20_KEYBYTES);
+  if (!init) {
+    randombytes(key, 32);
     init = 1;
   }
-  //crypto_stream(r,rlen,nonce,key);
-  crypto_stream_salsa20(r,rlen,nonce,key);
+  nfl_crypto_stream_salsa20_amd64_xmm6(r, rlen, nonce, key);
 
   // Increase 64-bit counter (nonce)
-  for(i=0;i<8;i++)
-    n ^= ((unsigned long long)nonce[i]) << 8*i;
+  for (i = 0; i < 8; i++) n ^= ((unsigned long long)nonce[i]) << 8 * i;
   n++;
-  for(i=0;i<8;i++)
-    nonce[i] = (n >> 8*i) & 0xff;
+  for (i = 0; i < 8; i++) nonce[i] = (n >> 8 * i) & 0xff;
 }
-
 }
