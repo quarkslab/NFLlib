2481de49e181a6b1a2f71a628a9983389569537a
serge-sans-paille <sguelton@quarkslab.com>
2017-07-24 09:04:13 +0200

    Prevent multiple definition error
    Fixes #24

---
+++ tests/multi0.cpp
@@ -0,0 +1,6 @@
+// compilation of this file with along multi1.cpp looks for multiple definitions
+
+#include <nfl.hpp>
+int main() {
+  return 0;
+}
---
+++ tests/multi1.cpp
@@ -0,0 +1,2 @@
+// compilation of this file along with multi0.cpp looks for multiple definitions
+#include <nfl.hpp>
